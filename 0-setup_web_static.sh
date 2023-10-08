6d882215656c32addf63428587af5eeea61523ab
Bunje Kelly <kellybunje@gmail.com>
2023-10-08 11:24:26 -0700

    fixes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,5 +18,5 @@ content="<html>
 echo "$content" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/server_name_ {/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "/server {/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
