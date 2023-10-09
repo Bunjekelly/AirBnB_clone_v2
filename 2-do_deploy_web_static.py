969adf7a755455c871f848a5117e48766bad9bf9
Bunje Kelly <kellybunje@gmail.com>
2023-10-08 17:09:48 -0700

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,15 +13,18 @@ def do_deploy(archive_path):
     if not os.path.isfile(archive_path):
         return False
     try:
-        archive_name = archive_path.split('/')[1]
+        archive_name = archive_path.split('/')[-1]
         name = archive_name.split('.')[0]
-        release_folder = "/data/web_static/releases/{}/".format(name)
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_folder))
+        put(archive_name, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'.format(name))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_name, name))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
         run('rm /tmp/{}'.format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+                "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(release_folder))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
         return True
     except:
         return False
