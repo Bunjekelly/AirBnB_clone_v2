653dc3aa06dbafc7f40e89c4571c9f9f8aab5213
Bunje Kelly <kellybunje@gmail.com>
2023-10-22 17:30:13 +0200

    added 3 tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,5 +12,16 @@ def hello_world():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
